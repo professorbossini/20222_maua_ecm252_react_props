123008697e7e1495d8f9c9dcfcfaf33db6727019
asbonato <asbonato@gmail.com>
2022-08-28 22:13:47 -0300

    apostila 4 até bloco 2.7.2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return <div className='container border rounded mt-2'>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        {/**linha para o título */}
+        <div className="row border-bottom m-2">
+            <h1 className='display-5 text-center'>Seus pedidos</h1>
+        </div>
+
+        {/**linha para o primeiro pedido */}
+        <div className='row'>
+            {/**controle de colunas para responsividade */}
+            <div className='col-sm-8 col-md-6 m-2'>
+                <Pedido data="22/04/2021" icone="fas fa-hdd fa-2x"
+                    titulo="SSD" descricao="SSD Kingston A400 - Sata"/>
+            </div>
+        </div>
+
+        {/**linha para o segundo pedido */}
+        <div className='row'>
+            {/**controle de colunas para responsividade */}
+            <div className='col-sm-8 col-md-6 m-2'>
+            <Pedido data="20/04/2021" icone="fas fa-book fa-2x"
+                    titulo="Livro" descricao="Concrete Mathematics - Donald Knuth"/>
+            </div>
+        </div>
+        
+        {/**linha para o terceiro pedido */}
+        <div className='row'>
+            {/**controle de colunas para responsividade */}
+            <div className='col-sm-8 col-md-6 m-2'>
+            <Pedido data="21/01/2021" icone="fas fa-laptop fa-2x"
+                    titulo="Notebook" descricao="Notebook Dell - 8Gb - i5"/>
+            </div>
+        </div>
+
+    </div>
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
